79d9b5a51ba0c57fb81bea46f230d339203c1cc7
Apic <dlsdud9098@naver.com>
2025-04-05 07:16:40 +0000

    asdf

--- programmers/files/level1-389478.py
+++ programmers/files/level1-389478.py
@@ -77,57 +77,68 @@ def create_stack():
 
     print(f'n: {n}, w: {w}, num: {num}')
     print(len(storage[stack_num]) - 1)
-    
+
 if __name__ == '__main__':
-    n = 89127356
-    w = 11275
-    num = 1237
-
-
+    n = 13
+    w = 3
+    num = 6
 
     # 몇 번째 라인인지는 모르지만 행이 1번째에 있음을 알 수 있음 
-    # print(n // num - 1)
-    num_line = num // w
-    if num % w == 0:
-        num_line -= 1
-    print('행',num_line)
+    # 6 12 18과 같이 딱 떨어지는 숫자여도 같은 행에 있으니 미리 -1
+    row_line = (num - 1) // w
+    print(row_line, '행')
 
-    prev = 0
-    start = 1
-
-
+    # 전체 행의 수
+    rows = n // w
     if n % w != 0:
-        stack_num = n // w + 1
+        rows += 1
+
+    
+    print(f'현재 위치: {row_line}행에 위치해 있고 그 행의 수는 {rows}칸')
+
+    # 열 위치 찾기
+    # 정방향 (왼 -> 오), 1 3 5 7 9 ...
+    if row_line % 2 == 0: 
+        column = (num - 1) % w
+    # 역방향 (오 -> 왼), 11 9 7 5 ....
     else:
-        stack_num = n // w
+        column = w - (num - 1) % w - 1
 
-    # 행이 짝수일 때 
-    if num_line % 2 == 0:
-        # 정방향에서부터 시작해서 8이기 때문에 앞에서 부터
-        column = num - w - 1
-    else:
-        # 역방향에서부터 시작해서 8 이기 때문에 뒤에서부터
-        column = w - (num - w)
+    if w * column > n:
+        rows -= 1
+        
+    storage = []
+    
+    for r in range(1, rows):
+        # 짝수 행 
+        if r % 2 == 0:
+            '''
+            현재 행의 값 
+            현재 행 : r => 0
+            전체 열의 수: w = > 짝수수이기 때문에 0 2 4 6...
+            해당 숫자가 있는 열 위치: column => 0 1 2 3 4 5 <= w-1
 
-    print('열',column)
+            값이 1부터 시작하기 때문에 +1
 
-    # storage = []
-    storage = [column + 1]
-    # print(storage)
-    now_num = 0
-    print(stack_num)
-    for i in range(1, stack_num):
-        print(i)
-        # print(i)
-        # 짝수이기 때문에 갈수록 더해지는 숫자가 줄어들음 
-        if i % 2 == 0:
-            now_num = 2 * w - (2 * w - 1 - (2 * column)) + storage[-1]
-            storage.append(now_num)
-            # print(i, 2*w - 1 - (2 * column))
+            ex) 2행인 경우
+            r(2행) * w(총 6개의 열) + column(4열) + 1
+            2행이기 때문에 최대 열(6) 의 2배인 12부터 시작
+            12로부터 열(4)만큼 더한 곳에 위치해 있음
+            숫자는 1부터 시작하기 때문에 마지막에 +1
+
+            ==> 2행 4열 ==> 17
+            '''
+            current_num = r * w + column + 1
         else:
-            now_num = 2*w - 1 - (2 * column) + storage[-1]
-            storage.append(now_num)
-            # print(i, 2 * w - (2 * w - 1 - (2 * column)))
+            '''
+            2행 이기 때문에 해당 행의 최대값은 2*열의 최대값 
+            (r + 1) * w ==> 2 * 6 == > 12
+            홀수 행은 오른쪽에서 왼쪽으로 숫자가 커지기 때문에 왼쪽에 있는 숫자가 가장 큼
+            오른쪽으로 갈 수록 숫자가 줄어들기 때문에 열의 수(4)만큼 작아짐 
+            '''
+            current_num = (r + 1) * w - column
+        storage.append(current_num)
+
     print(storage)
 
     print(len(storage) - storage.index(num))
