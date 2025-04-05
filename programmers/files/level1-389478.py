86905409611407a654da8c3216bb6d383d576ac3
Apic <dlsdud9098@naver.com>
2025-04-05 04:55:42 +0000

    asdf

--- programmers/files/level1-389478.py
+++ programmers/files/level1-389478.py
@@ -3,11 +3,7 @@ n: 택배 개수
 w: 가로 개수
 num: 꺼내려는 상자 개수
 '''
-
-if __name__ == '__main__':
-    n = 13
-    w = 3
-    num = 6
+def create_stack():
 
     storage = []
     # 가로 개수만큼 스택 만들기
@@ -21,6 +17,8 @@ if __name__ == '__main__':
 
     if n % w != 0:
         stack_num = n // w + 1
+    else:
+        stack_num = n // w
     # print('stack_num: ', stack_num)
 
     # 스택 만들 때 이미 1,2,3,4,5 ...는 넣었기 떄문에 1번 안해도 된다 
@@ -79,34 +77,57 @@ if __name__ == '__main__':
 
     print(f'n: {n}, w: {w}, num: {num}')
     print(len(storage[stack_num]) - 1)
+    
+if __name__ == '__main__':
+    n = 89127356
+    w = 11275
+    num = 1237
 
 
-    # 몇 번째 라인인지는 모르지만 1번째 칸에 있음을 알 수 있음 
+
+    # 몇 번째 라인인지는 모르지만 행이 1번째에 있음을 알 수 있음 
     # print(n // num - 1)
-    num_line = n // num - 1
+    num_line = num // w
+    if num % w == 0:
+        num_line -= 1
+    print('행',num_line)
 
     prev = 0
     start = 1
 
-    if num_line // 2 == 0:
-        start = 2*w - 1 + start
 
-        for idx, i in enumerate(range(start, 0, -2)):
-            if i == num:
-                column = idx
+    if n % w != 0:
+        stack_num = n // w + 1
     else:
-        start = 1 + start
+        stack_num = n // w
 
-    # print(column)
+    # 행이 짝수일 때 
+    if num_line % 2 == 0:
+        # 정방향에서부터 시작해서 8이기 때문에 앞에서 부터
+        column = num - w - 1
+    else:
+        # 역방향에서부터 시작해서 8 이기 때문에 뒤에서부터
+        column = w - (num - w)
 
-    stroage = []
+    print('열',column)
+
+    # storage = []
+    storage = [column + 1]
+    # print(storage)
     now_num = 0
-    while now_num < n:
-        for i in range(stack_num):
-            if i // 2 == 0:
-                now_num = (2*w - 1) + (i+1)
-                print(now_num)
-                storage.append(now_num)
-            else:
-                
-                
+    print(stack_num)
+    for i in range(1, stack_num):
+        print(i)
+        # print(i)
+        # 짝수이기 때문에 갈수록 더해지는 숫자가 줄어들음 
+        if i % 2 == 0:
+            now_num = 2 * w - (2 * w - 1 - (2 * column)) + storage[-1]
+            storage.append(now_num)
+            # print(i, 2*w - 1 - (2 * column))
+        else:
+            now_num = 2*w - 1 - (2 * column) + storage[-1]
+            storage.append(now_num)
+            # print(i, 2 * w - (2 * w - 1 - (2 * column)))
+    print(storage)
+
+    print(len(storage) - storage.index(num))
