60bd9144ae3d6c8401f7e057e4028fbbfe557899
Apic <dlsdud9098@naver.com>
2025-04-24 10:59:38 +0900

    Delete tempCodeRunnerFile.py

--- programmers/files/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-g = [log for i, log in enumerate(timelog) if (i + startday - 1) % 7 + 1 not in {6, 7}]
-        
-        # # print(timelog)
-        
-        # if timelog and all(log <= schedule + 10 for log in timelog):
-        #     answer += 1
