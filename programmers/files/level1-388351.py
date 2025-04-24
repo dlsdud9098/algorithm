222f72695e5241656ce6fa2b0abcc8fb77b0c320
Apic <dlsdud9098@naver.com>
2025-04-24 10:09:17 +0900

    Update level1-388351.py

--- programmers/files/level1-388351.py
+++ programmers/files/level1-388351.py
@@ -1,35 +1,25 @@
-if __name__ == '__main__':
-    schedules = [730, 855, 700, 720]
-    timelogs = [[710, 700, 650, 735, 700, 931, 912], 
-                [908, 901, 805, 815, 800, 831, 835], 
-                [705, 701, 702, 705, 710, 710, 711], 
-                [707, 731, 859, 913, 934, 931, 905]]
-    startday = 1
+def solution(schedules, timelogs, startday):
     answer = 0
-    
 
-    # print((i + startday - 1) % 7)
-    for schedule, timelog in zip(schedules, timelogs):
-        if not timelog:
-            continue
-        if schedule > 1100 and schedule < 700:
-            continue
-        if startday <1  and startday > 7:
-            continue
+    schedules = [(schedule - 50) + 100 if (schedule + 10) % 100 >= 60 else schedule + 10 for schedule in schedules ]
+
+    for idx, timelog in enumerate(timelogs):
+        for day, log in enumerate(timelog):
+            # 현재 날짜 계산
+            now_day = day+startday
+            if now_day > 7:
+                now_day -= 7
+
+            if (now_day != 7) and (now_day != 6):
+                # 출근 시간보다 일찍 오거나 정상 시간에 왔을 때
+                print(log, schedules[idx])
+                if log <= schedules[idx]:
+                    login = True
+                else:
+                    login = False
+                    break
         
-        # print(timelog)
-        for i, log in enumerate(timelog):
-            if (i + startday - 1) % 7 + 1 not in {6, 7}:
-                if not log <= schedule + 10:
-                    continue
-        answer += 1
-        
-        # print(timelog)
-        # timelog = [log for i, log in enumerate(timelog) if (i + startday - 1) % 7 + 1 not in {6, 7}]
-        
-        # # print(timelog)
-        
-        # if timelog and all(log <= schedule + 10 for log in timelog):
-        #     answer += 1
-        
-    print(answer)
+        if login:
+            answer += 1
+    
+    return answer
