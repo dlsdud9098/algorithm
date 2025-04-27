caa758cd44e3acacfb69d4b7c4533a9321fd8004
dlsdud9098 <dlsdud9098@naver.com>
2025-04-27 06:28:39 +0000

    adf

--- programmers/files/level1-340213.py
+++ programmers/files/level1-340213.py
@@ -1,42 +1,42 @@
-video_len, pos, op_start, op_end, commands, result = "34:33", "13:00", "00:55",	"02:55", ["next", "prev"], "13:00"
-video_len, pos, op_start, op_end, commands, result = "10:55",	"00:05",	"00:15",	"06:55",	["prev", "next", "next"],	"06:55"
-# video_len, pos, op_start, op_end, commands, result = "07:22",	"04:05",	"00:15",	"04:07",	["next"],	"04:17"
+def solution(video_len, pos, op_start, op_end, commands):
+    answer = ''
+    
+    # 분-초 -> 초로 바꾸기
+    def set_time(t):
+        mm, ss = t
+        return int(mm) * 60 + int(ss)
 
-# 시간을 숫자로 바꿈
-video_len = int(video_len.replace(':', ''))
-pos = int(pos.replace(':', ''))
-op_start = int(op_start.replace(':', ''))
-op_end = int(op_end.replace(':', ''))
+    # 모든 시간에 적용
+    video_len = set_time(video_len.split(':'))
+    pos = set_time(pos.split(':'))
+    op_start = set_time(op_start.split(':'))
+    op_end = set_time(op_end.split(':'))
 
-for com in commands:
-    # 현재 pos가 op_start와 op_end 사이에 있을 때
-    if (op_start <= pos) and (op_end >= pos):
-        pos = op_end
+    for com in commands:
+        # 현재 pos가 op_start와 op_end 사이에 있을 때
+        if (op_start <= pos) and (op_end >= pos):
+            pos = op_end
 
-    if com == 'next':
-        pos += 10
-        if pos >= video_len: 
-            pos = video_len
-            continue
-        # pos의 초가 60을 넘어갈 때
-        if (pos % 100 )> 59:
-            pos -= 60   # 60초 빼기 
-            pos += 100  # 1분 더하기 
+        # 명령어가 next일 때 
+        if com == 'next':
+            pos += 10
+            # 영상 길이 벗아닐 시 
+            if pos >= video_len: 
+                pos = video_len
+                continue
+        # 명령어가 prev 일 때 
+        else:
+            pos -= 10
+            # 영상 길이 벗어날 시 
+            if pos <= 0:
+                pos = 0
+                continue
 
+    # 초 -> 분-초
+    if pos % 60 > 59:
+        pos = (pos // 60) + (pos % 60)
 
-    else:
-        pos -= 10
-        if pos <= 0:
-            pos = 0
-            continue
-
-        if (pos % 100) > 59:
-            pos -=40
-    print(pos)
-if (op_start <= pos) and (op_end >= pos):
-    pos = op_end
-
-
-pos = f'{pos // 100:02d}:{pos % 100:02d}'
-
-print(result == pos)
+    pos = f'{pos // 60:02d}:{pos % 60:02d}'
+    
+    answer = pos
+    return answer
